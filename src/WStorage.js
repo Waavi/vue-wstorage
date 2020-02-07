9e1e856e843fba8cec9ce27199757699bb47fb70
alejandroledesma <alejandro.ledesma.dev@gmail.com>
2020-02-07 10:50:54 +0100

    HotFix in WStorage

--- src/WStorage.js
+++ src/WStorage.js
@@ -4,25 +4,7 @@ export default class WStorage extends Storage {
     constructor (props) {
         super(props)
 
-        this.localStore = new Storage({ ...props, storage: 'local' })
-        this.sessionStore = new Storage({ ...props, storage: 'session' })
-    }
-
-    /**
-    * Local store
-    * @returns {*}
-    * @public
-    */
-    local () {
-        return this.localStore
-    }
-
-    /**
-    * Session store
-    * @returns {*}
-    * @public
-    */
-    session () {
-        return this.sessionStore
+        this.local = new Storage({ ...props, storage: 'local' })
+        this.session = new Storage({ ...props, storage: 'session' })
     }
 }
